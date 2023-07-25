37acd223ff9f55c8b0d2f849c0327ec686ffcad5
Chris Anders <chris.anders@schwaebisch-hall.de>
2023-07-25 12:42:55 +0200

    Spieler Klasse hinzugefügt

--- Shared/Tischkicker.Models/src/java/Spieler.java
+++ Shared/Tischkicker.Models/src/java/Spieler.java
@@ -1,3 +1,20 @@
-class Spieler {
-    
+class Spieler : BasisKlasse {
+    private String name;
+    private String abteilung;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAbteilung() {
+        return this.abteilung;
+    }
+
+    public void setAbteilung(String abteilung) {
+        this.abteilung = abteilung;
+    }
 }
