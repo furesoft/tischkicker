a80c91c56885630c4c2b18e114b38965397fa8d4
Johannes Richter <j.richter@schwaebisch-hall.de>
2023-07-27 09:23:04 +0200

    Implement Tor Controller

---
+++ Server/src/main/java/de/shgruppe/tischkicker_server/controllers/TorController.java
@@ -0,0 +1,26 @@
+package de.shgruppe.tischkicker_server.controllers;
+import org.springframework.web.bind.annotation.*;
+import tischkicker.models.Spieler;
+
+
+@RestController
+public class TorController {
+
+
+
+    @PostMapping ("/tor")
+    public void torgeschossen() {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
