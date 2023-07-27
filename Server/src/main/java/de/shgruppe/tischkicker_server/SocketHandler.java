bea7c2ddcc4747f5be7caca4bc92f9e9a69c5d92
Felix Herrmann <felix.herrmann@schwaebisch-hall.de>
2023-07-27 10:38:53 +0200

    LiveSocket und WebsocketConfiguration hinzugefügt.

---
+++ Server/src/main/java/de/shgruppe/tischkicker_server/SocketHandler.java
@@ -0,0 +1,34 @@
+package de.shgruppe.tischkicker_server;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+import tischkicker.models.Tor;
+
+import javax.annotation.processing.Messager;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+
+@Component
+public class SocketHandler extends TextWebSocketHandler {
+
+    ArrayList<WebSocketSession> sessions = new ArrayList();
+
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message)
+            throws InterruptedException, IOException {
+
+        for(WebSocketSession webSocketSession : sessions) {
+            //Map value = new ().fromJson(message.getPayload(), Map.class);
+            webSocketSession.sendMessage(new TextMessage(message.getPayload()));
+        }
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        //the messages will be broadcasted to all users.
+        sessions.add(session);
+    }
+}
