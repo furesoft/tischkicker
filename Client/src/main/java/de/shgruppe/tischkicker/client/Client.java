142e005cde23525f7010377ee55a48c2d69fa2e8
Stilianos Vassiliadis <stilianos.vassiliadis@schwaebisch-hall.de>
2023-07-31 08:36:25 +0200

    Request Spiele

--- Client/src/main/java/de/shgruppe/tischkicker/client/Client.java
+++ Client/src/main/java/de/shgruppe/tischkicker/client/Client.java
@@ -1,5 +1,8 @@
 package de.shgruppe.tischkicker.client;
 import com.google.gson.Gson;
+import tischkicker.models.Spiel;
+import java.util.Arrays;
+import java.util.List;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -11,7 +14,7 @@ public class Client {
 
     public static void main(String[] args) {
         String apiUrl = "http://localhost:8080/teams"; // Hier die API-URL einsetzen
-
+        String apiUrl2 = "http://localhost:8080/spiele";
         try {
 
             // Gson-Objekt zum Serialisieren von JSON erstellen
@@ -29,16 +32,31 @@ public class Client {
                     .POST(HttpRequest.BodyPublishers.ofString(jsonData, StandardCharsets.UTF_8))
                     .build();
 
+            //HTTP-GET-Anfrage erstellen
+            HttpRequest requestSpieler = HttpRequest.newBuilder()
+                    .uri(URI.create(apiUrl2))
+                    .header("Content-Type", "application/json")
+                    .header("Accept", "application/json")
+                    .GET()
+                    .build();
+
+
+
             // Die Anfrage an die API senden und die Antwort erhalten
             HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> responseSpieler = httpClient.send(requestSpieler, HttpResponse.BodyHandlers.ofString());
 
             // Den HTTP-Statuscode der Antwort überprüfen
             int statusCode = response.statusCode();
             if (statusCode == 200) {
                 // Die JSON-Antwort verarbeiten
+                String responseBody2 = responseSpieler.body();
                 String responseBody = response.body();
                 System.out.println("API-Antwort:");
                 System.out.println(responseBody);
+
+                Spiel[] spielArray = gson.fromJson(responseBody2, Spiel[].class);
+                List<Spiel> spiele = Arrays.asList(spielArray);
             } else {
                 System.out.println("Fehler bei der API-Anfrage. Response Code: " + statusCode);
             }
@@ -47,7 +65,5 @@ public class Client {
             e.printStackTrace();
         }
     }
-
-
     }
 
