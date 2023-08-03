25a5c7fab8f77b3bfa93c8c4e3ad7dbc88ad34db
Chris Anders <chris.anders@schwaebisch-hall.de>
2023-08-03 14:48:39 +0200

    SpielManager kann jetzt erkennen wann ein Seitenwechsel stattfinden muss und wann ein Spiel gewonnen ist und gibt bei Spielende das Ergebnis an den Client weiter.

--- Server/src/main/java/de/shgruppe/tischkicker_server/logic/SpielManager.java
+++ Server/src/main/java/de/shgruppe/tischkicker_server/logic/SpielManager.java
@@ -1,12 +1,23 @@
 package de.shgruppe.tischkicker_server.logic;
 
+import de.shgruppe.tischkicker_server.repositories.SpielRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import tischkicker.models.Spiel;
 import tischkicker.models.SpielErgebnis;
 import tischkicker.models.Tor;
 
 public class SpielManager {
     public static SpielManager Instance = new SpielManager();
     private final SpielErgebnis ergebnis = new SpielErgebnis();
+
+    private final SpielPhase runde = new SpielPhase();
     private boolean tauscheTeams = false;
+
+    public int anzahltoreBisGewonnen = 10;
+    public boolean beachteSeitenwechsel = false;
+
+    @Autowired
+    SpielRepository spielRepository;
 
     private SpielManager() {
 
@@ -16,6 +27,12 @@ public class SpielManager {
         ergebnis.toreTeam2 = 0;
         ergebnis.toreTeam1 = 0;
         tauscheTeams = false;
+    }
+
+    public void spielStarten(Spiel spiel) {
+        ergebnis.spiel = spiel;
+
+        reset();
     }
 
     public void empfangeTor(Tor tor) {
@@ -34,6 +51,27 @@ public class SpielManager {
             else {
                 ergebnis.toreTeam2 += 1;
             }
+        }
+
+        triggerSpielMode();
+    }
+
+    /***
+     * Wenn Seitenwechsel aktiviert ist, wird bei der hälfte des maximums der Spiele um zu gewinnen ein Seitenwechsel durchgeführt.
+     * Wenn diese erreicht ist, ist das Spiel vorbei und wird in die Datenbank gespeichert.
+     */
+    private void triggerSpielMode() {
+        int maxTore = Math.max(ergebnis.toreTeam1, ergebnis.toreTeam2); // die größte Anzahl Tore der Teams holen, da diese relevant für den weiteren Schritt ist
+
+        if (beachteSeitenwechsel) {
+            if (maxTore == anzahltoreBisGewonnen / 2) {
+                seitenWechsel();
+            }
+        }
+
+        if (maxTore == anzahltoreBisGewonnen) {
+            Instance.runde.empfangeEndergebnis(ergebnis);
+            spielRepository.save(ergebnis.spiel);
         }
     }
 
