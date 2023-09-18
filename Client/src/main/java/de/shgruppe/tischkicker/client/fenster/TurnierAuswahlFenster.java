560fb1879dc198f6b3c1667471bc558b98f49333
Johannes Richter <j.richter@schwaebisch-hall.de>
2023-09-18 09:58:45 +0200

    Gui neu Repariert 2.0

--- Client/src/main/java/de/shgruppe/tischkicker/client/fenster/TurnierAuswahlFenster.java
+++ Client/src/main/java/de/shgruppe/tischkicker/client/fenster/TurnierAuswahlFenster.java
@@ -11,43 +11,54 @@ public class TurnierAuswahlFenster extends JFrame {
 
     public TurnierAuswahlFenster() {
 
+
         Turnier[] alleTurniere = API.getTurniere();
 
-        this.setSize(400, 400);
+        this.setSize(600, 600);
 
         JPanel panel = new JPanel();
-        panel.setSize(400, 400);
+        panel.setLayout(null);
+        panel.setBounds(0, 0, 600,400);
         panel.setBackground(new java.awt.Color(40, 44, 52));
         this.add(panel);
         panel.setVisible(true);
 
-        JButton turnierErstellenButton = new JButton("+");
-        turnierErstellenButton.setSize(50, 50);
+        JButton turnierErstellenButton = new JButton("Neues Turnier");
+
+        turnierErstellenButton.setBackground(new java.awt.Color(165, 171, 179));
+
         turnierErstellenButton.addActionListener(e -> {
             API.erstelleTurnier();
             new TeamsInitialisierenFenster().setVisible(true);
+
         });
 
 
-        turnierErstellenButton.setBounds(300, 100, 50, 50);
+
+        turnierErstellenButton.setBounds(300, 100, 150, 50);
         panel.add(turnierErstellenButton);
         turnierErstellenButton.setVisible(true);
 
-
-        JButton turnierButton = new JButton("Turnier anzeigen ");
-        turnierButton.setSize(50, 50);
-        turnierButton.setBackground(new java.awt.Color(165, 171, 179));
-        turnierButton.setBounds(100, 200, 150, 50);
-        panel.add(turnierButton);
-        turnierButton.setVisible(true);
 
 
         JComboBox turniereComboBox = new JComboBox(alleTurniere);
 
         panel.add(turniereComboBox);
         turniereComboBox.setBackground(Color.WHITE);
-        turniereComboBox.setBounds(100, 100, 150, 50);
+        turniereComboBox.setForeground(Color.WHITE);
+        turniereComboBox.setBounds(50, 100, 200, 50);
         turniereComboBox.setVisible(true);
+
+        JButton turnierButton = new JButton("Turnier anzeigen ");
+
+        turnierButton.setBackground(new java.awt.Color(165, 171, 179));
+        turnierButton.setBounds(50, 200, 150, 50);
+        panel.add(turnierButton);
+        turnierButton.setVisible(true);
+
+
+
     }
 
 }
+
