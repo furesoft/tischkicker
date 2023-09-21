9680142f41a3bd4fba8fd32401cb8b206657a85a
Chris Anders <chris.anders@schwaebisch-hall.de>
2023-09-21 14:02:21 +0200

    Hint hinzugefügt

--- Client/src/main/java/de/shgruppe/tischkicker/client/Hint.java
+++ Client/src/main/java/de/shgruppe/tischkicker/client/Hint.java
@@ -7,8 +7,11 @@ public class Hint {
     public static void enableFor(JTextField textbox, String hint) {
         Font defaultFont = textbox.getFont();
         Color defaultForeground = textbox.getForeground();
+        Font hintFont = new Font(textbox.getFont().getName(), Font.ITALIC, textbox.getFont().getSize());
 
+        textbox.setForeground(Color.GRAY);
         textbox.setText(hint);
-        textbox.setFont(new Font(textbox.getFont().getName(), Font.ITALIC, textbox.getFont().getSize()));
+        textbox.setFont(hintFont);
+        textbox.addFocusListener(new HintMouseListener(defaultFont, defaultForeground, textbox, hint, hintFont));
     }
 }
