c4a8cf0fa439106b52c3d2743ded369f1c79396c
Chris Anders <chris.anders@schwaebisch-hall.de>
2023-10-06 07:10:08 +0000

    Feature/vorsprung gewinn

--- Client/src/main/java/de/shgruppe/tischkicker/client/fenster/Siegertreppchen.java
+++ Client/src/main/java/de/shgruppe/tischkicker/client/fenster/Siegertreppchen.java
@@ -14,37 +14,40 @@ public class Siegertreppchen extends JFrame {
     JLabel treppchenPlatz2 = new JLabel("", SwingConstants.CENTER);
     JLabel treppchenPlatz3 = new JLabel("", SwingConstants.CENTER);
 
+    private Point posErster = new Point(640,620);
+    private Point posZweiter = new Point(1090,700);
+    private Point posDritter = new Point(190,800);
+
+    int width = 50;
+    int height = 50;
+    int textsize = 50;
+
     public Siegertreppchen() throws IOException {
-        int x = 15;
-        int y = 15;
-        int width = 800;
-        int height = 750;
+
+        // Bild ist 1305*900 groß
 
         setExtendedState(JFrame.MAXIMIZED_BOTH);
-        this.pack();
 
         JLabel jlb = new JLabel();
         jlb.setIcon(new ImageIcon(getClass().getClassLoader().getResourceAsStream("podium.png").readAllBytes()));
-        jlb.setSize(getWidth(), getHeight());
 
         add(jlb);
 
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-        treppchenPlatz2.setBounds(x, y, width, height);
         treppchenPlatz2.setForeground(Color.WHITE);
-        add(treppchenPlatz2);
+        treppchenPlatz2.setFont(new Font("Arial", 0,textsize));
+        jlb.add(treppchenPlatz2);
 
-        treppchenPlatz1.setLocation(getWidth() / 2 - 50, getHeight() - 50);
         treppchenPlatz1.setForeground(Color.WHITE);
-        add(treppchenPlatz1);
+        treppchenPlatz1.setFont(new Font("Arial", 0,textsize));
+        jlb.add(treppchenPlatz1);
 
-        treppchenPlatz3.setLocation(this.getWidth() / 2 - 50, this.getHeight() - 150);
         treppchenPlatz3.setForeground(Color.WHITE);
-        add(treppchenPlatz3);
+        treppchenPlatz3.setFont(new Font("Arial", 0,textsize));
+        jlb.add(treppchenPlatz3);
 
         setLocationRelativeTo(null);
-
         this.setResizable(false);
         this.setLocation(0, 0);
         this.pack();
@@ -53,7 +56,25 @@ public class Siegertreppchen extends JFrame {
     public void setTeams(List<Team> teams) {
         treppchenPlatz1.setText(teams.get(0).getName());
         treppchenPlatz2.setText(teams.get(1).getName());
-        treppchenPlatz3.setText(teams.get(2).getName());
+
+        if (teams.get(2) != null)
+        {
+            treppchenPlatz3.setText(teams.get(2).getName());
+            treppchenPlatz3.setBounds(posDritter.x - getTeamsWidth(treppchenPlatz3)/2 , posDritter.y,width,height);
+        }
+
+        treppchenPlatz1.setBounds(posErster.x - getTeamsWidth(treppchenPlatz1) / 2, posErster.y, width, height);
+        treppchenPlatz2.setBounds(posZweiter.x - getTeamsWidth(treppchenPlatz2) / 2, posZweiter.y, width, height);
+    }
+
+    //TODO
+    // Bug bei längeren Namen, werden dann einfach nur 3 Punkte angezeigt
+
+    public int getTeamsWidth(JLabel jLabel)
+    {
+        FontMetrics metrics = this.getGraphics().getFontMetrics(jLabel.getFont());
+
+        return SwingUtilities.computeStringWidth(metrics, jLabel.getText());
     }
 
 }
