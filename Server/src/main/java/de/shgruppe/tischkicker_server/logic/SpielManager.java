1d06338fe7a24f30ec88e1f04e309327039b98d9
Felix Herrmann <felix.herrmann@schwaebisch-hall.de>
2023-10-06 12:23:06 +0200

    Bug gefixt

--- Server/src/main/java/de/shgruppe/tischkicker_server/logic/SpielManager.java
+++ Server/src/main/java/de/shgruppe/tischkicker_server/logic/SpielManager.java
@@ -134,7 +134,7 @@ public class SpielManager {
         int maxTore = Math.max(ergebnis.toreTeam1, ergebnis.toreTeam2); // die größte Anzahl Tore der Teams holen, da diese relevant für den weiteren Schritt ist
         int aktuelleTordifferenz = Math.abs(ergebnis.spiel.getToreteam1() - ergebnis.spiel.getToreteam2()); // absolute tordifferenz
 
-        return aktuelleTordifferenz == TurnierManager.aktuellesTurnier.getTordifferenz() && maxTore >= anzahlToreBisGewonnen || ergebnis.teams[0].isAufgegeben() || ergebnis.teams[1].isAufgegeben();
+        return aktuelleTordifferenz == TurnierManager.aktuellesTurnier.getTordifferenz() || ergebnis.teams[0].isAufgegeben() || ergebnis.teams[1].isAufgegeben();
     }
 
     private void triggerSpielMode() throws IOException {
@@ -142,10 +142,10 @@ public class SpielManager {
         ergebnis.spiel.setToreteam2(team2.tore);
 
         if (hatSpielerGewonnen()) {
-            if (ergebnis.toreTeam1 == anzahlToreBisGewonnen || ergebnis.teams[1].isAufgegeben()) {
+            if (ergebnis.toreTeam1 >= anzahlToreBisGewonnen || ergebnis.teams[1].isAufgegeben()) {
                 ergebnis.spiel.setGewinnerID(ergebnis.teams[0].getId());
             }
-            if (ergebnis.toreTeam2 == anzahlToreBisGewonnen || ergebnis.teams[0].isAufgegeben()) {
+            if (ergebnis.toreTeam2 >= anzahlToreBisGewonnen || ergebnis.teams[0].isAufgegeben()) {
                 ergebnis.spiel.setGewinnerID(ergebnis.teams[1].getId());
             }
             spielRepository.saveAndFlush(ergebnis.spiel);
@@ -166,12 +166,12 @@ public class SpielManager {
 
                 SocketHandler.broadcast(tmsg);
 
-                /*
+
                 SiegerTreppchenMessage treppchenMessage = new SiegerTreppchenMessage();
                 treppchenMessage.teams = getTreppchenTeams();
 
                 SocketHandler.broadcast(treppchenMessage);
-                 */
+
 
                 stats.incrementTeamTore(ergebnis.teams[0], ergebnis.toreTeam1, ergebnis.toreTeam2);
                 stats.incrementTeamTore(ergebnis.teams[1], ergebnis.toreTeam2, ergebnis.toreTeam1);
