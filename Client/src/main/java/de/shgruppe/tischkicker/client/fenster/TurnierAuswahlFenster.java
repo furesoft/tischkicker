1716166ab9688cce782b0cbdd3901652cc7b6a0f
Chris Anders <chris.anders@schwaebisch-hall.de>
2023-10-09 08:52:48 +0200

    turnierauswahlfenster unterscheidung für startbutton hinzugefügt

--- Client/src/main/java/de/shgruppe/tischkicker/client/fenster/TurnierAuswahlFenster.java
+++ Client/src/main/java/de/shgruppe/tischkicker/client/fenster/TurnierAuswahlFenster.java
@@ -89,8 +89,7 @@ public class TurnierAuswahlFenster extends JFrame {
 
             aktuellesTurnier = alleTurniere.stream().filter(t -> t.getId() == turnier.getId()).findFirst().get();
 
-            var turnierSpiele = Arrays.stream(getSpieleFromServer()).filter(s -> s.getTurnierID() == turnier.getId())
-                                      .collect(Collectors.toList());
+            var turnierSpiele = getSpieleVonTurnier(turnier);
 
             Spiel[] spiele = API.startTurnier(turnier.getId());
             turnierbaumGenerieren(spiele);
@@ -103,12 +102,27 @@ public class TurnierAuswahlFenster extends JFrame {
         setLocationRelativeTo(null);
     }
 
+    private static List<Spiel> getSpieleVonTurnier(Turnier turnier) {
+        return Arrays.stream(getSpieleFromServer()).filter(s -> s.getTurnierID() == turnier.getId())
+                     .collect(Collectors.toList());
+    }
+
     private static void setButtonText(Turnier selectedTurnier, DataButton turnierStartenButton) {
         if (selectedTurnier.isGespielt()) {
             turnierStartenButton.setText("Turnier anzeigen");
         }
         else {
-            turnierStartenButton.setText("Turnier starten/fortsetzen");
+            List<Spiel> turnierSpiele = getSpieleVonTurnier(selectedTurnier);
+            boolean hatTurniergestartetesSpiel = turnierSpiele.stream()
+                                                              .filter(s -> s.getQualifikation() == 1 && s.getGewinnerID() > 0)
+                                                              .findAny().isPresent();
+
+            if (hatTurniergestartetesSpiel) {
+                turnierStartenButton.setText("Turnier fortsetzen");
+            }
+            else {
+                turnierStartenButton.setText("Turnier starten");
+            }
         }
     }
 }
