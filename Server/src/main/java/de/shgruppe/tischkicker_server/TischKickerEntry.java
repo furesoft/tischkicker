7e9417d9a3806c0b29abdc9fd2b05b89611d6ab3
Chris Anders <chris.anders@schwaebisch-hall.de>
2024-01-10 09:22:53 +0100

    test

--- Server/src/main/java/de/shgruppe/tischkicker_server/TischKickerEntry.java
+++ Server/src/main/java/de/shgruppe/tischkicker_server/TischKickerEntry.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+//test
+
 @SpringBootApplication
 @EntityScan("tischkicker.models")
 @EnableJpaRepositories("de.shgruppe.tischkicker_server.repositories")
